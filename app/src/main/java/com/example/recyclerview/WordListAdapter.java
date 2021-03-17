d761a093fcd797ddf4f3de93683b545980e5109e
stunnerkritika <thapakritika019@gmail.com>
2021-03-18 00:02:10 +0545

    Task 3 done

--- app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -27,7 +27,7 @@ public class WordListAdapter extends    RecyclerView.Adapter<WordListAdapter.Wor
         mInflater = LayoutInflater.from(context);
         this.mWordList = wordList;
     }
-    class WordViewHolder extends RecyclerView.ViewHolder {
+    class WordViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener  {
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
 
@@ -35,6 +35,20 @@ public class WordListAdapter extends    RecyclerView.Adapter<WordListAdapter.Wor
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
         }
     }
     @NonNull
